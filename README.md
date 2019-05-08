e83d53edf230c5c42f069e32235799290c0d6d6a
Hasnain Naeem <30462034+hasnainnaeem@users.noreply.github.com>
2019-05-09 01:58:15 +0500

    Updated PHP Contact Form

--- README.md
+++ README.md
@@ -1,13 +1,1 @@
-# Personal Web using Bootstrap 4.0
-## Some Features
-- Responsive Design
-- Resume
-- Contact form along with form validation
-- Twitter & Instagram Feed
-
-## Technologies Used
-- HTML
-- CSS
-- JS: JQUERY
-- PHP
-- Twitter Bootstrap 4.0
+# personal-web-lab-project-using-bootstrap
--- php/contact.php
+++ php/contact.php
@@ -1,3 +1,53 @@
 <?php
-    echo "<br /><b>Thank you " . $_POST['fname'] . " for contacting me! I'll get in touch with you soon.</b></p>"
-?>
+// site owner
+$site_name = 'Personal Website';
+$sender_domain = 'hasnainnaeemmk@gmail.com';
+$to = 'mnaeem.bscs17seecs@seecs.edu.pk';
+
+
+// contact form fields
+$name = htmlspecialchars(trim( $_POST['fname'] )) . ' ' . htmlspecialchars(trim( $_POST['lname'] ));
+$email = htmlspecialchars(trim( $_POST['email'] ));
+$subject = "Email from Personal Website";
+$message = htmlspecialchars(trim( $_POST['message'] ));
+
+
+// check for error
+$error = false;
+
+if ( $name === "" ) { $error = true; }
+
+if ( $email === "" ) { $error = true; }
+
+if ( $subject === "" ) { $error = true; }
+
+if ( $message === "" ) { $error = true; }
+
+
+// if no error, then send mail
+if ( $error == false )
+{
+    $body = "Name: $name \n\nEmail: $email \n\nMessage: $message";
+
+    $headers = "From: " . $site_name . ' <' . $sender_domain . '> ' . "\r\n";
+    $headers .= "Reply-To: " . $name . ' <' . $email . '> ' . "\r\n";
+
+    $mail_result = mail( $to, $subject, $body, $headers );
+
+    if ( $mail_result == true )
+    {
+        echo 'Mail successfully sent.';
+        echo "<br /><b>Thank you " . $_POST['fname'] . " for contacting me! I'll get in touch with you soon.</b></p>";
+    }
+    else
+    {
+        echo 'Mail could not be sent. Please try again.';
+    }
+    // end if
+}
+else
+{
+    echo 'error';
+}
+// end if
+
